33d19428dd0ce9f3a2f3783974900faa86e9574e
shiyasmohd <shiyasjaseena124@gmail.com>
2023-03-25 16:52:38 +0530

    Updates

---
+++ precache-manifest.73dd84d852b509588a80e884ba6913c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "59a9be56ddfb5267aa8c",
+    "url": "/secureX/static/css/main.92c6cf87.chunk.css"
+  },
+  {
+    "revision": "59a9be56ddfb5267aa8c",
+    "url": "/secureX/static/js/main.2e546185.chunk.js"
+  },
+  {
+    "revision": "d18b3d7343cd35e3ca4b",
+    "url": "/secureX/static/js/runtime~main.0724a787.js"
+  },
+  {
+    "revision": "9c587673ec83562b3381",
+    "url": "/secureX/static/css/2.a450240c.chunk.css"
+  },
+  {
+    "revision": "9c587673ec83562b3381",
+    "url": "/secureX/static/js/2.753379a5.chunk.js"
+  },
+  {
+    "revision": "609073e35415ba7b9e1e6b91fe181da0",
+    "url": "/secureX/index.html"
+  }
+];
